425c32f79f300df50d4c64fc38627108f6fdb375
Adam Proctor <aproctor@gmail.com>
2015-12-04 00:14:03 -0500

    part 2 day 4

--- day4.rb
+++ day4.rb
@@ -4,13 +4,13 @@
 
 require 'digest/md5'
 
-SAFETY = 1000000
+SAFETY = 10000000
 
-def find_lowest_num(key)
+def find_lowest_num(key, pattern)
 	num = 0
 	while(num < SAFETY) do
 		result = Digest::MD5.hexdigest("#{key}#{num}")
-		if(!result.match(/^00000/).nil?)
+		if(!result.match(pattern).nil?)
 			puts "Found  <#{result}> at num <#{num}>"
 			return num
 		end
@@ -22,5 +22,9 @@ def find_lowest_num(key)
 end
 
 puts "Part 1 Searching..."
-puts find_lowest_num("ckczppom")
-puts "Done"
+puts find_lowest_num("ckczppom",/^00000/)
+
+puts "\nPart 2 Searching..."
+puts find_lowest_num("ckczppom",/^000000/)
+
+puts "\nDone"
