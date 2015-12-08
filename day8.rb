63f7cd9c5ebe090b110056b29affba6a3d1ce680
Adam Proctor <adam@uken.com>
2015-12-08 12:13:49 -0500

    day 8 - new line fix

--- day8.rb
+++ day8.rb
@@ -8,13 +8,19 @@ non_escaped_char_count = 0
 File.open('day8.data').each do |line|
   continue if(line.nil?)
 
+  init_char_count = char_count
+
   line.each_char do |c|
     char_count += 1
   end
 
+  #each line has an extra \n at the end in our data file
+  char_count -= 1
+
   #Eval is evil, but whatevs
   evalString = eval(line)
   non_escaped_char_count += evalString.length
+
 end
 
 
