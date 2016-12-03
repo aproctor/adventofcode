13174339a70c60d926731db95c77c5232f28cde2
Adam Proctor <aproctor@gmail.com>
2016-12-03 11:29:11 -0500

    part 1 day 3

--- 2016/day3.rb
+++ 2016/day3.rb
@@ -4,18 +4,26 @@
 
 puts "Advent of Code 2016 day 3"
 
-
+valid = 0
 
 File.open('day3.data').each do |line|
   continue if(line.nil?)
 
-  line.each_char do |instruction|
+  #split data into side numbers
+  md = /(\d+)\s+(\d+)\s+(\d+)/.match(line)    
+  if(!md.nil? && md.length)    
+    a = md[1].to_i
+    b = md[2].to_i
+    c = md[3].to_i
 
- 
+    if(a+b > c && a+c > b && b + c > a)
+      valid += 1
+    end
   end
-
  
 end
+
+puts "part1 valid triangles = #{valid}"
 
 
 
