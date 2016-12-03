bfcf3dd81404fa49682d84f02962939d1b920703
Adam Proctor <aproctor@gmail.com>
2016-12-03 11:12:10 -0500

    day3 stub

---
+++ 2016/day3.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# Day 1 2016
+# See http://adventofcode.com/2016/day/3
+
+puts "Advent of Code 2016 day 3"
+
+
+
+File.open('day3.data').each do |line|
+  continue if(line.nil?)
+
+  line.each_char do |instruction|
+
+ 
+  end
+
+ 
+end
+
+
+
+
+
+
