edfb9dfd161a10aa9b1f2e55764bd2386f3566b9
Adam Proctor <aproctor@gmail.com>
2016-12-04 01:40:52 -0500

    finished p1, don't count sector characters

--- 2016/day4.rb
+++ 2016/day4.rb
@@ -13,11 +13,11 @@ File.open('day4.data').each do |line|
   #split data into side numbers
   matched_chars = {}
 
-  md = /-(\d+)\[(.*)\]/.match(line) 
-  sector = md[1].to_i
-  checksum = md[2]
+  md = /([a-z\-]+)-(\d+)\[(.*)\]/.match(line) 
+  sector = md[2].to_i
+  checksum = md[3]
 
-  line.each_char do |c|
+  md[1].each_char do |c|
     next if c == '-'
 
     if(matched_chars.key?(c))
@@ -27,7 +27,7 @@ File.open('day4.data').each do |line|
     end
   end
 
-  #sort by count, then alphabetical, then take the first 5 keys and make a checksum string
+  #sort by count (desc), then alphabetical (asc), then take the first 5 keys and make a checksum string
   top_chars = matched_chars.sort_by { |letter, count| [-count,letter]}.to_h.keys[0..4].join('')
   if(top_chars == checksum)
     p1_sum += sector
