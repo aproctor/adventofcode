0492d20a6344fd15487a2888b792b9854d1b664b
Adam Proctor <aproctor@gmail.com>
2016-12-16 02:15:24 -0500

    revert part 1 code back

--- 2016/day16.rb
+++ 2016/day16.rb
@@ -5,70 +5,52 @@
 puts "Advent of Code 2016 day 16"
 
 initial_state = "01110110101001000"
-DISK_SIZE = 35651584
+DISK_SIZE = 272
 
 MAX_ITERATIONS = 10000000
 
-def dragon_string(a)      
+def dragon_string(a)    
   b = a.reverse
 
-  result = [a,"0"]
+  result = a + "0"
   b.each_char do |c|
     if(c == "0")
-      result << "1"
+      result += "1"
     else
-      result << "0"
+      result += "0"
     end
   end    
   
-  result.join('')
+  result
 end
 
 #assumes an even length string for valid results
 def checksum(input)  
-  limit = input.length
-  
-  MAX_ITERATIONS.times do |x|
-    check = []
-    chunk = []
-    if(limit % 2 != 0)
-      return input
-    end
-    
-
-    j = 0    
-    check_flag = false
-    limit.times do |i|      
-      if(check_flag)
-        #replace the input inline, save memory
-        chunk << ((input[i-1] == input[i]) ? "1" : "0")
-        j += 1        
-      end
-
-      check_flag = !check_flag
-      if(i % 10000 == 0 && i > 0)  
-        check << chunk.join('')
-        chunk = []
+  check = ""
+  i = 0
+  prev_char = ""
+  input.each_char do |c|
+    if(i % 2 == 1)
+      if(prev_char == c)
+        check = check + "1"
+      else
+        check = check + "0"
       end
     end
-    if(chunk.length > 0)
-      check << chunk.join('')
-    end
 
-    limit = j
-    input = check.join('')
-    puts "C #{limit}"    
+    prev_char = c
+    i += 1
   end
 
-  puts "Unable to find checksum"
-  return nil
-end
+  if(check.length % 2 == 0)
+    return checksum(check)
+  end
 
-# puts checksum("110010110100")
+  return check
+end
 
 output = initial_state
 MAX_ITERATIONS.times do |i|
-  puts "#{i} - #{output.length} / #{DISK_SIZE} #{(100*output.length.to_f/DISK_SIZE).round(2)}%" # if i % 10 == 0
   if(output.length >= DISK_SIZE)
     output = output[0..DISK_SIZE-1]
     break
@@ -77,5 +59,5 @@ MAX_ITERATIONS.times do |i|
   end
 end
 
-puts "Checksum: *#{checksum(output)}*"
-
+puts "Output: #{output}"
+puts "Checksum: #{checksum(output)}"
