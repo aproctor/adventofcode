d39d3d4aaf31a46c2ebe2af5445e738fdc20e1ca
Adam Proctor <aproctor@gmail.com>
2016-12-20 21:22:27 -0500

    base linked list implementation

--- 2016/day19.rb
+++ 2016/day19.rb
@@ -15,5 +15,47 @@ def josephus_winner(n)
   return 2*l + 1
 end
 
-p1_num = 3017957
-puts "Part 1 - with #{p1_num} elves #{josephus_winner(p1_num)} wins"
+def brute_forcephus_winner(n)
+  #Present stealing has been moved to across from current elf
+  #we could do the math and come up with a new proof... or we could just run through the numbers
+
+  #build a linked list of all numbers
+  first_elf = nil
+  last_elf = nil
+  n.times do |i|
+    elf = Elf.new(i+1)
+    if(first_elf.nil?)
+      first_elf = elf
+    end
+    if(!last_elf.nil?)
+      last_elf.next = elf
+    end
+
+    last_elf = elf
+  end
+  #point last elf at first elf to complete the circle
+  last_elf.next = first_elf
+
+  elf = first_elf
+  while(elf.next != elf)
+    #puts "kill #{elf.next.value}"
+    next_next = elf.next.next
+    elf.next = next_next
+
+    elf = elf.next
+  end
+
+  return elf.value
+end
+
+class Elf
+  attr_accessor :value, :next
+
+  def initialize(value)
+      @value = value
+  end
+end
+
+num_elves = 41 #3017957
+puts "Part 1 - with #{num_elves} elves #{josephus_winner(num_elves)} wins"
+puts "Part 2 - with #{num_elves} elves #{brute_forcephus_winner(num_elves)} wins"
