2919a021169f179049141de46274c2f7123a02c3
Adam Proctor <aproctor@gmail.com>
2017-12-10 23:51:56 -0500

    day 9 part 2

--- 2017/day9.rb
+++ 2017/day9.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env ruby
 
-def p1_score_stream(stream)
+def score_stream(stream)
 	score = 0
 	depth = 0
 	in_garbage = false
 	in_negation = false
+	garbage_count = 0
 
 
 	stream.each_char do |c|
@@ -16,6 +17,8 @@ def p1_score_stream(stream)
 				in_garbage = false
 			elsif c == '!'
 				in_negation = true
+			else
+				garbage_count += 1
 			end
 
 			next
@@ -36,12 +39,14 @@ def p1_score_stream(stream)
 		end
 	end
 
+	puts "garbage count: #{garbage_count}"
+
 	score
 end
 
 File.open('day9.data').each do |line|
 	continue if(line.nil?)  
 
-	puts "Part 1 - #{p1_score_stream(line)}"
+	puts "Part 1 - #{score_stream(line)}"
 end
 
