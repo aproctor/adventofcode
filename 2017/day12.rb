e4d1a9df661fb73231b5c3cdb90b32326df013ce
Adam Proctor <aproctor@gmail.com>
2017-12-12 00:44:27 -0500

    day 12 p2 brute force and wrong answer

--- 2017/day12.rb
+++ 2017/day12.rb
@@ -4,11 +4,13 @@ puts "Day 12"
 
 class Program
 	attr_reader :pid, :pipe_keys, :pipes
+	attr_accessor :group
 
 	def initialize(pid, pipe_keys)
 		@pid = pid
 		@pipe_keys = pipe_keys
 		@pipes = {}
+		@group = nil
 	end
 
 	def connect_pipes(program_map)
@@ -63,9 +65,31 @@ end
 connected = 0
 program_map.each do |key, program|
 	path = program.path_to("0",[])
-	puts path.inspect if !path.nil?
+	#puts path.inspect if !path.nil?
 	connected += 1 if !path.nil?
 end
 
 puts "Part 1 - total connected #{connected}"
 
+#Part 2 find distinct groups
+group_roots = {}
+cur_group = 0
+group_roots[cur_group] = program_map.keys.first
+
+program_map.each do |key, program|
+	group_roots.each do |k,v|
+		if(!program.path_to(v,[]).nil?)
+			program.group = k
+			break
+		end
+	end
+	if(program.group.nil?)
+		puts "No group found for #{key}, #{cur_group}"
+		#no existing group found, let's define a new one
+		cur_group += 1
+		program.group = cur_group
+		group_roots[cur_group] = program
+	end
+end
+
+puts "Part 2 - #{group_roots.count} groups found"
