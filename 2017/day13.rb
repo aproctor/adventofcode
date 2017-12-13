fa9949145aa2c628700658c689337737eb90feaf
Adam Proctor <aproctor@gmail.com>
2017-12-13 00:44:12 -0500

    day 13 part 2 brute force search

--- 2017/day13.rb
+++ 2017/day13.rb
@@ -29,3 +29,29 @@ def p1_severity(depths)
 end
 
 puts "Part 1 - #{p1_severity(depths)}"
+
+MAX_ITERATIONS = 10000000
+START_OFFSET = 1000000
+def p2_sneaky(depths)
+	distance = depths.keys.last + 1	
+	(START_OFFSET..MAX_ITERATIONS).each do |delay|
+		caught = false
+		distance.times do |x|						
+			if(!depths[x].nil?)
+				#firewall here, count the offset of the sentry and if it's at 0 you're caught
+				patrol_length = depths[x] * 2 - 2
+				if((x + delay) % patrol_length == 0)
+					#caught, severity increased by depth * range
+					puts "Caught! (#{delay}) #{x} * #{depths[x]}"
+					caught = true
+					break
+				end
+			end
+		end
+		return delay if(!caught)
+	end
+
+	-1 #invalid timeout
+end
+
+puts "Part 2 - #{p2_sneaky(depths)}"
