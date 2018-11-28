03cb06b9a6468f75be20b6166bac1d0907109512
Adam Proctor <adam@uken.com>
2018-11-28 18:33:52 -0500

    stub 2018 '

---
+++ 2018/day1.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# Day 1 2018
+# See http://adventofcode.com/2018/day/1
+
+puts "Advent of Code 2018 day 1"
+
+def part_1(input)
+  return_val = 0
+
+  return_val
+end
+
+puts "Part 1"
+
+def part_2(input)
+  return_val = 0
+
+  return_val
+end
+
+puts "\nPart 2"
