ea667d913b7e18711b40890b07f16e5fd88de821
Adam Proctor <adam@uken.com>
2018-12-01 00:39:33 -0500

    day 1 2018

--- 2018/day1.rb
+++ 2018/day1.rb
@@ -2,20 +2,35 @@
 # Day 1 2018
 # See http://adventofcode.com/2018/day/1
 
+require "set"
+
 puts "Advent of Code 2018 day 1"
 
-def part_1(input)
-  return_val = 0
-
-  return_val
-end
 
 puts "Part 1"
-
-def part_2(input)
-  return_val = 0
-
-  return_val
+p1_value = 0
+instructions = []
+File.open('day1.data').each do |line|
+    continue if(line.nil?)
+    instructions << line.to_i
 end
 
-puts "\nPart 2"
+
+visited = {}
+first_repeat = nil
+
+10000.times do |i|
+  instructions.each do |n|
+    p1_value += n
+    if(visited[p1_value].to_i > 0)
+      first_repeat = p1_value
+      break
+    end
+    visited[p1_value] = p1_value
+  end
+  puts "p1: #{p1_value}" if i == 0
+  break if first_repeat != nil
+end
+
+puts "Part 2"
+puts "#{first_repeat}"
