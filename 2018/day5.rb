329c7664bc4bd2833c4c7c246e4bf3993affa8e2
Adam Proctor <adam@uken.com>
2018-12-05 18:09:43 -0500

    day5 linked list

--- 2018/day5.rb
+++ 2018/day5.rb
@@ -9,8 +9,6 @@ File.open('day5.data').each do |line|
   input = line
 end
 
-# input = "dabAcCaCBAcCcaDA"
-
 def reacting_chars(c1,c2)
   return !c2.nil? && c1.downcase == c2.downcase && c1 != c2
 end
@@ -56,4 +54,66 @@ def reduce(value)
   return nil
 end
 
-reduce(input)
+class Node
+  attr_accessor :char, :prev, :next
+
+  def initialize(char, prev)
+    @char = char
+    @prev = prev
+    @prev.next = self unless @prev.nil?
+  end
+
+  def react!
+    if !@next.nil? && reacting_chars(@char, @next.char)
+      @next.destruct
+      self.destruct
+      return true
+    elsif !@prev.nil? && reacting_chars(@char, @prev.char)
+      @prev.destruct
+      self.destruct
+      return true
+    end
+
+    return false
+  end
+
+  def destruct
+    # puts "Goodbye: #{@char}"
+    @prev.next = @next
+    @next.prev = @prev
+  end
+end
+
+def list_reduce(value)
+  root = Node.new("^", nil)
+  last_node = root
+  value.each_char do |c|
+    new_node = Node.new(c, last_node)
+    last_node = new_node
+  end
+  last_node.next = Node.new("$", last_node)
+
+  #iterate over list until no conflicts
+  n = root
+  loop do
+    n.react!
+
+    n = n.next
+    break if n == nil
+  end
+
+  #count length
+  n = root
+  count = 0
+  loop do
+    # puts n.char
+    n = n.next
+    count += 1
+    break if n == nil
+  end
+  puts "count: #{count-2}"
+end
+
+# input = "dabAcCaCBAcCcaDA"
+# reduce(input)
+list_reduce(input)
