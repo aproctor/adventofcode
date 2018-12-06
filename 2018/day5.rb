606ff6bba3ba5b79a6ad86b0f1b41ec84d620b0f
Adam Proctor <aproctor@gmail.com>
2018-12-05 23:19:38 -0500

    cleanup of day 5 p1

--- 2018/day5.rb
+++ 2018/day5.rb
@@ -13,47 +13,6 @@ def reacting_chars(c1,c2)
   return !c2.nil? && c1.downcase == c2.downcase && c1 != c2
 end
 
-def reduce(value)
-
-  MAX_ITERATIONS.times do |depth|
-    puts depth if depth % 1000 == 0
-
-    # puts "v: #{value}"
-
-    reaction = false
-    prev_char = nil
-
-    if depth > MAX_ITERATIONS
-      puts "We dug to deep! panic!"
-      return nil
-    end
-
-    buffer = []
-    value.each_char.with_index do |c,i|
-      if reacting_chars(c, prev_char)
-        reaction = true
-        #splice out these chars
-        buffer << value[0..i-2] if(i > 2)
-        buffer << value[i+1..-1] if(i < value.length - 1)
-        break
-      end
-      prev_char = c
-    end
-    if reaction
-      # start again
-      value = buffer.join('')
-    else
-      puts "#{value}"
-      puts "min value found: #{value.length}"
-
-      return value
-    end
-  end
-
-  puts "Nothing found after #{depth} iterations"
-  return nil
-end
-
 class Node
   attr_accessor :char, :prev, :next
 
@@ -117,5 +76,4 @@ def list_reduce(value)
 end
 
 # input = "dabAcCaCBAcCcaDA"
-# reduce(input)
 list_reduce(input)
