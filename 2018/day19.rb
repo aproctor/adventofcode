29bcc21f46884010cd09819eacd83e62b15ed325
Adam Proctor <adam@uken.com>
2019-11-20 14:19:08 -0500

    file generation and 2019 stubbed

---
+++ 2018/day19.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# See http://adventofcode.com/2019/day/19
+
+File.open('day19.data').each do |line|
+  next if(line.nil?)
+  md = line.match(/day ([0-9]+)/)
+  if(!md.nil?)
+    puts md[1]
+  end
+end
+
+# class Node
+#   attr_accessor :val
+
+#   def initialize(val)
+#     @val = val
+#   end
+# end
