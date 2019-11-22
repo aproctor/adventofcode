5f4eb97b904293eeb07d5918b08305053a00dd52
Adam Proctor <aproctor@gmail.com>
2019-11-22 00:53:23 -0500

    more class fixes

--- 2018/day11.rb
+++ 2018/day11.rb
@@ -7,6 +7,8 @@ class AreaGrid
     @serial = serial
     @grid_width = w
     @grid_height = h
+
+    @power_hash = {}
   end
 
   def power_level(x, y)
@@ -28,7 +30,7 @@ class AreaGrid
     # Subtract 5 from the power level.
     power -= 5
 
-    #explicit return not needed, but i don't like implicit returns
+    #explicit return not needed, but i don't like implicit returns    
     return power
   end
 
@@ -45,12 +47,17 @@ class AreaGrid
   end
 
   def area_power(x,y, area_w, area_h)
+    # key = "#{x},#{y},#{area_w},#{area_h}"
+    # return @power_hash[key] if @power_hash.key?(key)
+
     total_power = 0
     area_h.times do |i|
       area_w.times do |j|
         total_power += power_level(x+j, y+i)
       end
     end
+
+    # @power_hash[key] = total_power
 
     return total_power
   end
@@ -59,9 +66,9 @@ class AreaGrid
     max = -99999
     top_power = nil
 
-    mr = @grid_height - area_h
-    mc = @grid_width - area_w
-
+    mr = @grid_height - area_h + 1
+    mc = @grid_width - area_w + 1
+    
     (1..mr).each do |r|
       (1..mc).each do |c|
         x = c+1
@@ -84,14 +91,14 @@ class AreaGrid
   end
 
   def p2_power
-    cur_max = [-99999]
+    top_power = [-99999]
 
     (1..@grid_width).each do |w|
       (1..@grid_width).each do |h|
-        mp = max_power(serial, w,h)
+        mp = max_power(w,h)
 
-        if mp[0] > cur_max[0]
-          cur_max = mp
+        if mp[0] > top_power[0]
+          top_power = mp
         end
         # puts "#{w}x#{h} checked"
       end
@@ -108,4 +115,4 @@ end
 # AreaGrid.new(7672,300,300).p1_power
 
 puts "Part 2:"
-#AreaGrid.new(7672,300,300).p2_power
+AreaGrid.new(7672,300,300).p2_power
