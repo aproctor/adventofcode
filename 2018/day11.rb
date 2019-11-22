71b4b3eaf7a3ce204af6736aeb82789df678952f
Adam Proctor <adam@uken.com>
2019-11-22 13:45:30 -0500

    memoization but slow

--- 2018/day11.rb
+++ 2018/day11.rb
@@ -46,23 +46,50 @@ class AreaGrid
     end
   end
 
-  def area_power(x,y, area_w, area_h)
-    # key = "#{x},#{y},#{area_w},#{area_h}"
-    # return @power_hash[key] if @power_hash.key?(key)
+  def area_power(x,y, size)   
+    #Area of 1x1 is a base case, just return power level 
+    return power_level(x,y) if size == 1
+
+    key = "#{x},#{y},#{size}"
+
+    # puts "whats: #{key}?"
+
+    #found a value in the cache? return it
+    return @power_hash[key] if @power_hash.key?(key)
 
     total_power = 0
-    area_h.times do |i|
-      area_w.times do |j|
-        total_power += power_level(x+j, y+i)
-      end
+
+    #simple hack, we know we're going from small to large, so size of x-1 should be cached, let's grab that
+    #this should only recurse with a depth of 1
+    total_power += area_power(x,y, size-1)
+
+    #now let's measure ethe power of the extra row and column
+    edge_row = y + size - 1
+    edge_col = x + size - 1
+
+    # puts "Edge Row: #{edge_row}, Edge Col: #{edge_col}"
+
+    #measure row
+    (x..edge_col).each do |rx|
+      total_power += power_level(rx, edge_row)
+      # puts "row add (#{rx}, #{edge_row}) #{power_level(rx, edge_row)}"
+    end    
+
+    #measure col (exclusive, since the corner is already counted)
+    (y...edge_row).each do |ry|
+      total_power += power_level(edge_col, ry)
+      # puts "col add (#{edge_col}, #{ry}) #{power_level(edge_col, ry)}"
     end
 
-    # @power_hash[key] = total_power
+    @power_hash[key] = total_power
+
+    # puts "ap[#{key}] = #{total_power}"
 
     return total_power
   end
 
-  def max_power(area_w, area_h)
+  def max_power(size)
+    area_w = area_h = size
     max = -99999
     top_power = nil
 
@@ -73,10 +100,10 @@ class AreaGrid
       (1..mc).each do |c|
         x = c+1
         y = r+1
-        power = area_power(x,y, area_w, area_h)      
+        power = area_power(x,y, size)      
         if power > max
           max = power
-          top_power = [max, x, y, area_w, area_h]
+          top_power = [max, x, y, size]
         end
       end
     end
@@ -85,26 +112,25 @@ class AreaGrid
   end
 
   def p1_power
-    top_power = max_power(3, 3)
+    top_power = max_power(3)
     debug_grid(top_power[0]-1, top_power[1]-1, 5, 5)
-    puts "Max power #{top_power[0]} at: (#{top_power[1]},#{top_power[2]}) #{top_power[3]}x#{top_power[4]}"
+    puts "Max power #{top_power.shift} at: (#{top_power.join(',')})"
   end
 
   def p2_power
     top_power = [-99999]
 
     (1..@grid_width).each do |w|
-      (1..@grid_width).each do |h|
-        mp = max_power(w,h)
+      h = w
+      mp = max_power(w)
 
-        if mp[0] > top_power[0]
-          top_power = mp
-        end
-        # puts "#{w}x#{h} checked"
+      if mp[0] > top_power[0]
+        top_power = mp
       end
+      puts "#{w}x#{h} checked"
     end
 
-    puts "Max power #{top_power[0]} at: (#{top_power[1]},#{top_power[2]}) #{top_power[3]}x#{top_power[4]}"
+    puts "Max power #{top_power.shift} at: (#{top_power.join(',')})"
   end
 
 end
@@ -115,4 +141,5 @@ end
 # AreaGrid.new(7672,300,300).p1_power
 
 puts "Part 2:"
-AreaGrid.new(7672,300,300).p2_power
+AreaGrid.new(18,300,300).p2_power
+# AreaGrid.new(7672,300,300).p2_power
