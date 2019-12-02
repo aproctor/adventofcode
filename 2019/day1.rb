1113f527bc846d4bf419342c4d5915a529e51a0f
Adam Proctor <adam@uken.com>
2019-12-02 14:36:59 -0500

    2019 d1 p1

--- 2019/day1.rb
+++ 2019/day1.rb
@@ -1,13 +1,22 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2019/day/1
 
+def fuel_level(mass)
+  (mass / 3).floor - 2
+end
+
+# puts fuel_level(12)
+# puts fuel_level(14)
+# puts fuel_level(1969)
+# puts fuel_level(100756)
+
+total_fuel = 0
 File.open('day1.data').each do |line|
   next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
-  if(!md.nil?)
-    puts md[1]
-  end
+  
+  total_fuel += fuel_level(line.to_i)
 end
+puts "Total fuel: #{total_fuel}"
 
 # class Node
 #   attr_accessor :val
