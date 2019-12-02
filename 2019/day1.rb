df3a7a56bfa12c5cd352b575ed77a0cbea938afb
Adam Proctor <adam@uken.com>
2019-12-02 16:22:08 -0500

    day1 p2

--- 2019/day1.rb
+++ 2019/day1.rb
@@ -1,27 +1,33 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2019/day/1
 
-def fuel_level(mass)
+def p1_fuel_level(mass)
   (mass / 3).floor - 2
 end
 
-# puts fuel_level(12)
-# puts fuel_level(14)
-# puts fuel_level(1969)
-# puts fuel_level(100756)
+def p2_fuel_level(mass)
+  return 0 if(mass <= 0)
 
-total_fuel = 0
+  fuel = [(mass / 3).floor - 2,0].max
+  return fuel + p2_fuel_level(fuel)
+end
+
+# puts p1_fuel_level(12)
+# puts p1_fuel_level(14)
+# puts p1_fuel_level(1969)
+# puts p1_fuel_level(100756)
+
+# puts p2_fuel_level(14)
+# puts p2_fuel_level(1969)
+# puts p2_fuel_level(100756)
+
+p1_total_fuel = 0
+p2_total_fuel = 0
 File.open('day1.data').each do |line|
   next if(line.nil?)
   
-  total_fuel += fuel_level(line.to_i)
+  p1_total_fuel += p1_fuel_level(line.to_i)
+  p2_total_fuel += p2_fuel_level(line.to_i)
 end
-puts "Total fuel: #{total_fuel}"
-
-# class Node
-#   attr_accessor :val
-
-#   def initialize(val)
-#     @val = val
-#   end
-# end
+puts "P1 fuel: #{p1_total_fuel}"
+puts "P2 fuel: #{p2_total_fuel}"
