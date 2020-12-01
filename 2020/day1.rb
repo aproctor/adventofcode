9d7192b62d3acb070cbb8bc4fb567989c38807f3
Adam Proctor <adam@uken.com>
2020-12-01 09:46:53 -0500

    2020 day1 part1

--- 2020/day1.rb
+++ 2020/day1.rb
@@ -1,13 +1,25 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2020/day/1
 
+vals = []
 File.open('day1.data').each do |line|
   next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
-  if(!md.nil?)
-    puts md[1]
-  end
+  vals << line.to_i
 end
+
+product = 0
+vals.each_with_index do |v1, i|
+	vals.each_with_index do |v2, j|
+		next if i == j
+
+		if(v1 + v2 == 2020)
+			product = v1 * v2
+			break
+		end
+	end
+end
+
+puts "Part 1: #{product}"
 
 # class Node
 #   attr_accessor :val
