d24386c541edc197f54f8c6184c908fae5f57e95
Adam Proctor <adam@uken.com>
2020-12-03 00:14:15 -0500

    day3 part 1

--- 2020/day3.rb
+++ 2020/day3.rb
@@ -1,13 +1,29 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2020/day/3
 
+cur_pos = 0
+x_vel = 3
+tree_count = 0
+
 File.open('day3.data').each do |line|
   next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
-  if(!md.nil?)
-    puts md[1]
+  i = cur_pos % (line.length - 1) # ignore \n
+
+  x = line[i]
+  if(x == "#")
+  	line[i] = "X"
+  	tree_count += 1
+  else
+  	line[i] = "O"
   end
+  puts line
+
+
+  #move for next line
+  cur_pos += x_vel
 end
+
+puts "Trees hit: #{tree_count}"
 
 # class Node
 #   attr_accessor :val
