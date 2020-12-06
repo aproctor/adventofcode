15b73bfd1ec9d216e533d428acb91f4c5a8c18e2
Adam Proctor <aproctor@gmail.com>
2020-12-06 08:20:58 -0500

    day6 p1

--- 2020/day6.rb
+++ 2020/day6.rb
@@ -1,18 +1,45 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2020/day/6
 
-File.open('day6.data').each do |line|
-  next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
-  if(!md.nil?)
-    puts md[1]
+
+class DeclarationForm  
+
+  def initialize()
+  	@members = []
+  	@yes_vals = {}
+  end
+
+  def add_person(claim_string)
+  	claim_string.each_char do |c|
+  		@yes_vals[c] = 1
+  	end
+  	@members << claim_string
+  end
+
+  def yes_count
+  	#puts @members.inspect
+  	@yes_vals.keys.length
   end
 end
 
-# class Node
-#   attr_accessor :val
 
-#   def initialize(val)
-#     @val = val
-#   end
-# end
+forms = []
+form = DeclarationForm.new
+File.open('day6.data').each do |line|
+  next if(line.nil?)
+  
+  if(line == "\n")
+  	forms << form
+  	form = DeclarationForm.new
+  else
+  	form.add_person(line.strip)
+  end
+end
+#Add last form
+forms << form
+
+p1_sum = 0
+forms.each do |f|
+	p1_sum += f.yes_count
+end
+puts "Part 1: #{p1_sum}"
