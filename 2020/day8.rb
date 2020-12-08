f7d9d6b94feca1014399553028ddb87ab320d6f2
Adam Proctor <aproctor@gmail.com>
2020-12-08 08:26:16 -0500

    day 8 p1

--- 2020/day8.rb
+++ 2020/day8.rb
@@ -1,18 +1,43 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2020/day/8
 
+MAX_ITERATIONS = 10000
+
+#Part 1
+instructions = []
+cur_instruction = 0
+acc = 0
+
 File.open('day8.data').each do |line|
   next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
-  if(!md.nil?)
-    puts md[1]
-  end
+
+  instructions << line.split(' ')
 end
 
-# class Node
-#   attr_accessor :val
+visited_addresses = {}
+MAX_ITERATIONS.times do |x|
+	if(visited_addresses.key?(cur_instruction))
+		puts "Already executed instruction on line #{cur_instruction}"
+		break
+	end
+	if(cur_instruction < 0 || cur_instruction >= instructions.length)
+		puts "Out of bounds, freak out! #{cur_instruction}"
+		break
+	end
+	visited_addresses[cur_instruction] = 1
 
-#   def initialize(val)
-#     @val = val
-#   end
-# end
+	ins = instructions[cur_instruction]
+	puts "#{cur_instruction}: #{ins.inspect} | #{x}"
+	if(ins[0] == "acc")
+		acc += ins[1].to_i
+		cur_instruction += 1
+	elsif(ins[0] == "nop")
+		cur_instruction += 1
+	elsif(ins[0] == "jmp")
+		cur_instruction += ins[1].to_i
+	else
+		puts "Unrecognized command #{ins.inspect}"
+		break
+	end
+end
+puts "Part 1: #{acc}"
