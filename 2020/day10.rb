da21ec7bdbbb26e5856a3ba6da33590f8951ac4f
Adam Proctor <adam@uken.com>
2020-12-10 10:12:25 -0500

    day 10 part 1

--- 2020/day10.rb
+++ 2020/day10.rb
@@ -1,13 +1,38 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2020/day/10
 
+#start with outlet (joltage = 0)
+numbers = [0]
 File.open('day10.data').each do |line|
   next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
+  md = line.match(/([0-9]+)/)
   if(!md.nil?)
-    puts md[1]
+    numbers << md[1].to_i
   end
 end
+
+numbers.sort!
+
+# add device (highest joltage +3)
+numbers << numbers[-1] + 3
+
+
+puts "Part 1"
+three_count = 0
+one_count = 0
+(0..numbers.length-2).each do |i|
+	delta = numbers[i+1] - numbers[i]
+	if(delta > 3)
+		puts "Invalid sequence, can't continue from #{numbers[i]} to #{numbers[i+1]}"
+	elsif(delta == 3)
+		three_count += 1
+	elsif(delta == 1)
+		one_count += 1
+	end
+end
+puts "#{three_count} 3-jolt jumps * #{one_count} 1-jolt jumps = #{three_count*one_count}"
+
+
 
 # class Node
 #   attr_accessor :val
