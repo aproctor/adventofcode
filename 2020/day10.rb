268b440817ce5ece9007fe9d0a5fe3c23a6b586c
Adam Proctor <adam@uken.com>
2020-12-10 13:50:20 -0500

    day10 part 2

--- 2020/day10.rb
+++ 2020/day10.rb
@@ -20,17 +20,32 @@ numbers << numbers[-1] + 3
 puts "Part 1"
 three_count = 0
 one_count = 0
+max_length = 0
+cur_length = 0
+permute_map = [1,1,1,2,4,7]
+total_combos = 1
+
 (0..numbers.length-2).each do |i|
+	cur_length += 1
 	delta = numbers[i+1] - numbers[i]
 	if(delta > 3)
 		puts "Invalid sequence, can't continue from #{numbers[i]} to #{numbers[i+1]}"
 	elsif(delta == 3)
 		three_count += 1
+
+		total_combos *= permute_map[cur_length]
+
+		max_length = cur_length if cur_length > max_length
+		cur_length = 0		
 	elsif(delta == 1)
 		one_count += 1
 	end
 end
 puts "#{three_count} 3-jolt jumps * #{one_count} 1-jolt jumps = #{three_count*one_count}"
+#puts "Max Length: #{max_length}"
+
+puts "Part 2"
+puts "Total Combos: #{total_combos}"
 
 
 
