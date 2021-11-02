e438bdffa010dd533b842505b9f59fc6bafdfdd7
Adam Proctor <adam@uken.com>
2021-11-02 14:33:06 -0400

    2020 day 11 partial

--- 2020/day11.rb
+++ 2020/day11.rb
@@ -1,13 +1,19 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2020/day/11
 
+data = []
+
 File.open('day11.data').each do |line|
   next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
+  md = line.match(/([L\.]+)/)
   if(!md.nil?)
-    puts md[1]
+  	row = md[1].strip.split("")
+  	data << row
   end
 end
+
+puts data.inspect
+puts data.length
 
 # class Node
 #   attr_accessor :val
