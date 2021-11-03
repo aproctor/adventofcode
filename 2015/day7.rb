2543dc377cca5a72736bb86a3d1bd5aff7035cc0
Adam Proctor <adam@uken.com>
2021-11-03 16:09:50 -0400

    2015 day7 p1

--- 2015/day7.rb
+++ 2015/day7.rb
@@ -16,34 +16,95 @@ puts "Advent of Code 2015 day 7"
 # NOT x -> h
 # NOT y -> i
 
+class Node
+  attr_accessor :output_key, :input
+  @output_val = nil
+  @@node_map = {}
 
-def value_of(val, reg)
-  return reg[val] if(reg.key?(val))
+  def initialize(input, out)
+    @output_key = out
+    @input = input
 
-  val.to_i
-end
+    @@node_map[out] = self
+  end
 
-def not_16bit(val)
-  #Ruby doens't have 16-bit integers so we can't just use the ~ operator
-  bit_string = val.to_s(2).rjust(16,"0")
-  return_value = 0
+  def self.find(out)
+    (@@node_map.key?(out)) ? @@node_map[out] : nil
+  end
 
-  #puts "Value: #{val}"
-  #puts "Bits: #{bit_string}"
+  def self.value_of(val)
+    return @@node_map[val].output if(@@node_map.key?(val))
 
-  # Flipping the bits
-  bit_string.each_char.with_index do |c, i|
-    if c == '0'
-      return_value += 2 ** (15 - i)
+    #Not a known node, assume an integer.  Hacky but whatever, will be 0 for unknowns
+    val.to_i
+  end
+
+  def self.not_16bit(val)
+    #Ruby doens't have 16-bit integers so we can't just use the ~ operator
+    bit_string = val.to_s(2).rjust(16,"0")
+    return_value = 0
+
+    #puts "Value: #{val}"
+    #puts "Bits: #{bit_string}"
+
+    # Flipping the bits
+    bit_string.each_char.with_index do |c, i|
+      if c == '0'
+        return_value += 2 ** (15 - i)
+      end
+    end
+
+    #puts "Rval: #{return_value.to_s(2).rjust(16,'0')}"
+
+    return_value
+  end
+
+  def self.print_nodes
+    @@node_map.each do |k,v|
+      puts "#{k}: #{v.input}"
     end
   end
 
-  #puts "Rval: #{return_value.to_s(2).rjust(16,'0')}"
+  def self.print_values
+    @@node_map.each do |k,v|
+      puts "#{k}: #{v.output}"
+    end
+  end
 
-  return_value
+
+  def output
+    #memoize the value to avoid expensive reruns on wires
+    return @output_val unless @output_val.nil?
+
+    #puts input.inspect
+
+    if @input.length == 1
+      # 123
+      # x
+      @output_val = Node::value_of(@input[0])
+    elsif @input.length == 2
+      # NOT x
+      @output_val = Node::not_16bit(Node::value_of(@input[1]))
+    elsif @input.length == 3
+      # x AND y
+      # x OR y
+      # x LSHIFT 2
+      # y RSHIFT 2
+      if(@input[1] == "AND")
+        @output_val = Node::value_of(@input[0]) & Node::value_of(@input[2])
+      elsif(@input[1] == "OR")
+        @output_val = Node::value_of(@input[0]) | Node::value_of(@input[2])
+      elsif(@input[1] == "LSHIFT")
+        @output_val = Node::value_of(@input[0]) << @input[2].to_i
+      elsif(@input[1] == "RSHIFT")
+        @output_val = Node::value_of(@input[0]) >> @input[2].to_i
+      end    
+    end 
+
+    @output_val
+  end
 end
 
-registers = {}
 File.open('day7.data').each do |line|
   next if(line.nil?)
 
@@ -57,39 +118,15 @@ File.open('day7.data').each do |line|
   exit -1 if !valid
 
   command = line.strip.split(" -> ")
-  ouput = command[1]
+  output = command[1]
   input = command[0].split(" ")
-
-  #puts input.inspect
-  value = nil
-
-  if input.length == 1
-    # 123
-    # x
-    value = value_of(input[0], registers)
-  elsif input.length == 2
-    # NOT x
-    value = not_16bit(value_of(input[1], registers))
-  elsif input.length == 3
-    # x AND y
-    # x OR y
-    # x LSHIFT 2
-    # y RSHIFT 2
-    if(input[1] == "AND")
-      value = value_of(input[0], registers) & value_of(input[2], registers)
-    elsif(input[1] == "OR")
-      value = value_of(input[0], registers) | value_of(input[2], registers)
-    elsif(input[1] == "LSHIFT")
-      value = value_of(input[0], registers) << input[2].to_i
-    elsif(input[1] == "RSHIFT")
-      value = value_of(input[0], registers) >> input[2].to_i
-    end    
-  end
-  if(!value.nil?)
-    #puts "#{value} -> #{ouput}"
-    registers[ouput] = value
-  end 
+  Node.new(input, output)
 end
-puts "Registers: \n#{registers.inspect}"
 
-puts "Part 1: #{registers['a']}"
+# Node::print_nodes
+# Node::print_values
+# puts Node.find("x").output
+
+a_node = Node.find('a')
+p1_value = a_node.output
+puts "Part 1: #{p1_value}"
