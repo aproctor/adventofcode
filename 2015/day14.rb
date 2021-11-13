797b492949c3b6ebe5d0bb7421011a4bfdb72b12
Adam Proctor <adam@uken.com>
2021-11-12 19:19:28 -0500

    day 14

--- 2015/day14.rb
+++ 2015/day14.rb
@@ -1,18 +1,39 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2015/day/14
 
-File.open('day14.data').each do |line|
-  next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
-  if(!md.nil?)
-    puts md[1]
+class Reindeer
+  attr_accessor :given_name, :speed, :duration, :rest_time
+
+  def initialize(given_name, speed, duration, rest_time)
+    @given_name = given_name
+    @speed = speed
+    @duration = duration
+    @rest_time = rest_time
+  end
+
+  def travel_distance(time)
+  	total_distance = 0
+
+  	unit_time = duration + rest_time
+  	units = (time / unit_time).floor
+  	complete_time = unit_time * units
+  	# puts "#{time} - #{unit_time}, #{units}, #{complete_time}"
+
+  	total_distance += units * speed * duration
+   	total_distance += ([time - complete_time,duration].min) * speed
+
+   	puts "#{@given_name} travels #{total_distance}km after #{time}s"   	
+
+  	total_distance
   end
 end
 
-# class Node
-#   attr_accessor :val
+# Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+comet = Reindeer.new("Comet", 14, 10, 127)
 
-#   def initialize(val)
-#     @val = val
-#   end
-# end
+# Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+dancer = Reindeer.new("Dancer", 16, 11, 162)
+
+comet.travel_distance(2503)
+dancer.travel_distance(2503)
+
