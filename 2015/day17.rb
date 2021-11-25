de7a2e482b51e352bb35c9e3446f21342a2ba28e
Adam Proctor <adam@uken.com>
2021-11-25 15:17:48 -0500

    day 17 way too long

--- 2015/day17.rb
+++ 2015/day17.rb
@@ -1,13 +1,43 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2015/day/17
 
-File.open('day17.data').each do |line|
-  next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
-  if(!md.nil?)
-    puts md[1]
-  end
+
+def part1(target, containers)
+	matches = {}
+
+	indexes = []
+	containers.length.times do |i|
+		indexes[i] = i
+	end
+
+	indexes.permutation.each_with_index do |combo, i|
+		puts "#{i}" if i % 1000000 == 0
+		total = 0
+
+		used = []
+		combo.each do |bin|
+			val = containers[bin]
+			total += val
+			used << "#{val}:#{bin}"
+			break if total >= target
+		end
+
+		if(total == target) 
+			# puts "Combo #{combo} -> using #{used} = #{target}"
+			key = used.sort.join(',')
+			puts "Match Found: #{key}"
+			matches[key] = total
+		end	
+	end
+
+	puts "#{matches.size} unique matches"
+	matches.each do |k, v|
+		puts "\t#{k} = #{v}"
+	end
 end
+
+# part1(25, [20, 15, 10, 5, 5])
+part1(150, [11, 30, 47, 31, 32, 36, 3, 1, 5, 3, 32, 36, 15, 11, 46, 26, 28, 1, 19, 3])
 
 # class Node
 #   attr_accessor :val
