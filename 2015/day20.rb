11cc3de088cd6c9c8938c17385a5a70f4113bca6
Adam Proctor <aproctor@gmail.com>
2021-11-26 21:48:38 -0500

    day 20 part 1

--- 2015/day20.rb
+++ 2015/day20.rb
@@ -1,18 +1,48 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2015/day/20
 
-File.open('day20.data').each do |line|
-  next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
-  if(!md.nil?)
-    puts md[1]
-  end
+MAX_LOOP = 100000000
+
+def all_factors(n)
+	return [1] if n == 1
+
+	factors = [1,n]
+
+	mid_factor = Math.sqrt(n).floor
+	(2..mid_factor).each do |i|
+		if n % i == 0
+			factors << i			
+
+			# Add second factor, unless it's itself
+			b = n/i
+			factors << n/i if(i != b)
+		end
+	end
+
+	factors
 end
 
-# class Node
-#   attr_accessor :val
+def present_score(house)
+	total = 0	
+	all_factors(house).each do |i|
+		# puts " - #{i}"
+		total += i*10
+	end
 
-#   def initialize(val)
-#     @val = val
-#   end
-# end
+	total
+end
+
+def part1(target_score)
+	MAX_LOOP.times do |i|
+		s = present_score(i+1)
+		puts "House #{i+1} got #{s} presents."		
+
+		return i+1 if(s >= target_score)
+	end
+
+	nil
+end
+
+
+# part1(150)
+part1(36000000)
