e4c84a02201a17fa921314800ef2e60902575bbf
Adam Proctor <adam@uken.com>
2021-12-07 00:17:23 -0500

    day7 p1

--- 2021/day7.rb
+++ 2021/day7.rb
@@ -1,18 +1,41 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2021/day/7
 
+crabs = []
+min_val = 999999
+max_val = 0
 File.open('day7.data').each do |line|
   next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
-  if(!md.nil?)
-    puts md[1]
+  line.split(',').each do |v|
+  	i = v.to_i
+  	
+  	crabs << i
+
+  	min_val = i if i < min_val
+  	max_val = i if i > max_val
   end
 end
 
-# class Node
-#   attr_accessor :val
+puts "Crabs: #{crabs.join(',')}"
 
-#   def initialize(val)
-#     @val = val
-#   end
-# end
+# sum = crabs.inject(0, :+)
+# avg = sum / crabs.length
+# puts "Average position: #{avg}"
+
+min_fuel_required = 999999
+optimal_index = nil
+(min_val .. max_val).each do |i|
+	fuel_required = 0
+	crabs.each do |n|
+		fuel_required += (i - n).abs
+	end
+
+	if fuel_required < min_fuel_required
+		optimal_index = i
+		min_fuel_required = fuel_required
+	end
+
+	# puts "#{i}: #{fuel_required}"
+end
+
+puts "Optimal fuel required (#{min_fuel_required}) at position #{optimal_index}"
