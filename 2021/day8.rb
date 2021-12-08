f7da78d70716f9a013e7e884ab30f3a02d93a594
Adam Proctor <adam@uken.com>
2021-12-08 08:54:11 -0500

    day 8 p1

--- 2021/day8.rb
+++ 2021/day8.rb
@@ -1,18 +1,47 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2021/day/8
 
-File.open('day8.data').each do |line|
-  next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
-  if(!md.nil?)
-    puts md[1]
+
+class Display
+  attr_accessor :slot, :sequences
+
+  def initialize(slot, sequences)
+  	@slot = slot
+    @sequences = sequences
+  end
+
+  def add_readout(readout)
+  	@sequences << readout
   end
 end
 
-# class Node
-#   attr_accessor :val
 
-#   def initialize(val)
-#     @val = val
-#   end
-# end
+readouts = []
+displays = []
+digit = 0
+File.open('day8.data').each do |line|
+  next if(line.nil?)
+  parts = line.split(' | ')
+  displays << Display.new(digit, parts[0].split(' '))
+
+  readout = parts[1].split(' ')  
+  readouts << readout
+
+  digit += 1
+end
+
+# TODO maybe add in etra info from the readouts to the digits to better decrypt?
+
+puts "Part 1"
+one_four_seven_eight_count = 0
+readouts.each do |r|
+	r.each do |digit_signals|
+		l = digit_signals.length
+
+		if l == 2 || l == 4 || l == 3 || l == 7
+			one_four_seven_eight_count += 1
+		end
+	end
+end
+puts "Total 1, 4, 7 or 8 characters in readouts: #{one_four_seven_eight_count}"
+
