83c9bd7e37e3cda3d6e80422f96253d1123f62d4
Adam Proctor <aproctor@gmail.com>
2022-11-28 10:20:29 -0500

    2022 stub

---
+++ 2022/day3.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# See http://adventofcode.com/2022/day/3
+
+File.open('day3.data').each do |line|
+  next if(line.nil?)
+  md = line.match(/day ([0-9]+)/)
+  if(!md.nil?)
+    puts md[1]
+  end
+end
+
+# class Node
+#   attr_accessor :val
+
+#   def initialize(val)
+#     @val = val
+#   end
+# end
