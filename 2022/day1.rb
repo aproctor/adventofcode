4a26cdae032f01c92bba106fae6bf1a9a39a05aa
Adam Proctor <adam@uken.com>
2022-12-01 06:07:05 -0500

    day1 2022

--- 2022/day1.rb
+++ 2022/day1.rb
@@ -1,18 +1,28 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2022/day/1
 
+elfs = []
+total_cals = 0
+
 File.open('day1.data').each do |line|
   next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
+  md = line.match(/([0-9]+)/)
   if(!md.nil?)
-    puts md[1]
+    total_cals += md[1].to_i
+  else
+  	elfs << total_cals
+  	total_cals = 0
   end
 end
 
-# class Node
-#   attr_accessor :val
+if total_cals > 0
+	elfs << total_cals
+end
 
-#   def initialize(val)
-#     @val = val
-#   end
-# end
+elfs.sort!
+
+p1 = elfs[-1]
+puts "Part 1: #{p1} cals"
+
+p2 = elfs[-1] + elfs[-2] + elfs[-3]
+puts "Part 2: #{p2} cals"
