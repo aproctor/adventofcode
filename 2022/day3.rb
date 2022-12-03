48716524f0689e73ea46ef4c9b7b982717b29838
Adam Proctor <aproctor@gmail.com>
2022-12-03 01:55:42 -0500

    day3 p2

--- 2022/day3.rb
+++ 2022/day3.rb
@@ -26,11 +26,44 @@ def bag_sum(contents)
   0
 end
 
-total = 0
-File.open('day3.data').each do |line|
-  next if(line.nil?)
+def badge_value(group_bags)
+  # puts "Group bags check #{group_bags}"
+  existing = {}
+  group_bags[0].each_char do |c|
+    existing[c] = 1
+  end
 
-  total += bag_sum(line.strip) if line.length > 1
+  common = {}
+  group_bags[1].each_char do |c|
+    common[c] = 1 if existing.key? c
+  end
+
+  group_bags[2].each_char do |c|
+    if common.key? c
+      # puts "#{c} exists in all 3 group_bags"
+      return letter_value(c)
+    end
+  end
+
+  0
 end
 
-puts "Total: #{total}"
+p1_total = 0
+p2_total = 0
+bags = []
+File.open('day3.data').each do |line|
+  next if(line.nil?)
+  line.strip!
+
+  p1_total += bag_sum(line) if line.length > 1
+
+  bags << line
+  if(bags.length == 3)
+    p2_total += badge_value(bags)
+    bags = []
+  end
+end
+
+puts "Part 1: #{p1_total}"
+puts "Part 2: #{p2_total}"
+
