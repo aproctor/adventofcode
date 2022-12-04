a059a5a6355516ee0601dfb904ca92ecadbc53f2
Adam Proctor <aproctor@gmail.com>
2022-12-04 07:10:27 -0500

    day4 p1

--- 2022/day4.rb
+++ 2022/day4.rb
@@ -1,18 +1,33 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2022/day/4
 
-File.open('day4.data').each do |line|
-  next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
-  if(!md.nil?)
-    puts md[1]
+
+class Elf
+  attr_accessor :min, :max
+
+  def initialize(min, max)
+    @min = min
+    @max = max
+  end
+
+  def contains(other)
+    (@min <= other.min && @max >= other.max)
   end
 end
 
-# class Node
-#   attr_accessor :val
+p1_total = 0
+File.open('day4.data').each do |line|
+  next if(line.nil?)
+  md = line.match(/([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)/)
+  if(!md.nil?)
+    # puts md
+    elf1 = Elf.new(md[1].to_i, md[2].to_i)
+    elf2 = Elf.new(md[3].to_i, md[4].to_i)
+    if elf1.contains(elf2) || elf2.contains(elf1)
+      # puts "Overlap!"
+      p1_total += 1
+    end
+  end
+end
 
-#   def initialize(val)
-#     @val = val
-#   end
-# end
+puts "Part 1: #{p1_total}"
