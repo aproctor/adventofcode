3263f4b288485e7ec2998f721625f4da8d7a7a71
Adam Proctor <aproctor@gmail.com>
2022-12-05 01:25:32 -0500

    day4 p2 uncommited

--- 2022/day4.rb
+++ 2022/day4.rb
@@ -10,12 +10,17 @@ class Elf
     @max = max
   end
 
-  def contains(other)
+  def overlaps?(other)
+    !(@max < other.min || @min > other.max)
+  end
+
+  def contains?(other)
     (@min <= other.min && @max >= other.max)
   end
 end
 
 p1_total = 0
+p2_total = 0
 File.open('day4.data').each do |line|
   next if(line.nil?)
   md = line.match(/([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)/)
@@ -23,11 +28,17 @@ File.open('day4.data').each do |line|
     # puts md
     elf1 = Elf.new(md[1].to_i, md[2].to_i)
     elf2 = Elf.new(md[3].to_i, md[4].to_i)
-    if elf1.contains(elf2) || elf2.contains(elf1)
-      # puts "Overlap!"
+    if elf1.contains?(elf2) || elf2.contains?(elf1)
+      # puts "contains!"
       p1_total += 1
+    end
+
+    if elf1.overlaps?(elf2)
+      # puts "Overlap"
+      p2_total += 1
     end
   end
 end
 
 puts "Part 1: #{p1_total}"
+puts "Part 2: #{p2_total}"
