3ee783a94103bf08d7a7252b4718e12dc5f6192f
Adam Proctor <aproctor@gmail.com>
2022-12-06 07:25:55 -0500

    day 6 p2

--- 2022/day6.rb
+++ 2022/day6.rb
@@ -1,23 +1,23 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2022/day/6
 
-def start_of_packet(line)
-  puts line
+def start_of_packet(line, seq_len)
+  # puts line
 
   sequence = []
   line.each_char.with_index do |c, i|
-    sequence.shift if i >= 4
+    sequence.shift if i >= seq_len
     sequence << c
     
     unique_sequence = true
-    if sequence.length == 4
+    if sequence.length == seq_len
       #validate uniqueness
       sequence.tally.each do |k,v|
         unique_sequence = false if v > 1
       end
 
       if unique_sequence
-        puts "Unique sequence found from #{i-4} to #{i}: #{sequence.join('')}"
+        # puts "Unique sequence found from #{i-seq_len} to #{i}: #{sequence.join('')}"
         return i+1
       end
     end
@@ -27,13 +27,17 @@ def start_of_packet(line)
   nil
 end
 
-# puts start_of_packet("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
-# puts start_of_packet("bvwbjplbgvbhsrlpgdmjqwftvncz")
-# puts start_of_packet("nppdvjthqldpwncqszvftbrmjlhg")
-# puts start_of_packet("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
-# puts start_of_packet("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+# puts start_of_packet("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14)
+# puts start_of_packet("bvwbjplbgvbhsrlpgdmjqwftvncz", 14)
+# puts start_of_packet("nppdvjthqldpwncqszvftbrmjlhg", 14)
+# puts start_of_packet("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14)
+# puts start_of_packet("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14)
 
 File.open('day6.data').each do |line|
   next if(line.nil?)
-  puts start_of_packet(line)
+  puts "Part 1:"
+  puts start_of_packet(line, 4)
+
+  puts "\nPart 2:"
+  puts start_of_packet(line, 14)
 end
