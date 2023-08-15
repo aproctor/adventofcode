3f8f80d05bc320b4831d0ead3c79297ae8a4b662
Adam Proctor <aproctor@gmail.com>
2023-08-15 14:47:57 -0400

    day 10 part 2

--- 2022/day10.rb
+++ 2022/day10.rb
@@ -5,19 +5,34 @@ class Machine
   attr_accessor :i, :x, :next_eval, :acc
 
   def initialize
-    @i = 1
+    @i = 0
     @x = 1
-    @next_eval = 20
+    @next_eval = 40
     @acc = 0
+    @line = ""
   end
 
   def evaluate
+    cur_x = @i % 40
+    if(cur_x >= @x-1 && cur_x <= @x+1)
+      @line << "#"
+    else
+      @line << " "
+    end
+
     if(@i >= self.next_eval)
-      puts "#{@i} x #{@x} = #{@i*@x}"
+      #puts "#{@i} x #{@x} = #{@i*@x}"
       self.next_eval += 40
 
       @acc += @i*@x
+
+      @line = ""
+      print "#{i}:\t#{@line}\n"
+    else
+      print "#{i}:\t#{@line}\r"
     end
+    $stdout.flush
+    #sleep(0.01)
   end
 
   def parse(path)
@@ -31,7 +46,6 @@ class Machine
         @i += 1
         self.evaluate
 
-
         #process add command
         if command == :addx
           value = md[2].to_i
@@ -42,8 +56,6 @@ class Machine
         end
 
         #puts "#{i}: #{command} #{value} | #{x}"
-
-        self.evaluate
       end
     end
   end
@@ -52,6 +64,6 @@ end
 
 m = Machine.new
 m.parse('day10.data')
-puts "Part 1: #{m.acc}"
+#puts "Part 1: #{m.acc}"
 
 
