1d57de10de01d718aefd0a993aa862d34df06779
Adam Proctor <aproctor@gmail.com>
2023-12-04 09:39:15 -0500

    day1 p1

--- 2023/Godot/Solutions/Day1/day1.gd
+++ 2023/Godot/Solutions/Day1/day1.gd
@@ -3,4 +3,24 @@ extends Day
 class_name Day1
 
 func part_1(input) -> String:
-	return "My custom part 1"
+	var total = 0;
+	var output = ""
+		
+	var first_char_regex = RegEx.new()
+	first_char_regex.compile("^[a-z]*(\\d)")
+	
+	var last_char_regex = RegEx.new()
+	last_char_regex.compile("(\\d)[a-z]*$")
+		
+	for input_line in input.split("\n"):
+		if(input_line.length() > 0):
+			print(input_line)
+			var first_result = first_char_regex.search(input_line)
+			var last_result = last_char_regex.search(input_line)
+			if(first_result && last_result):
+				output += "%s-> %s%s\n" % [input_line, first_result.get_string(1), last_result.get_string(1)]
+				total += int("%s%s" % [first_result.get_string(1), last_result.get_string(1)])
+	
+	output += "Total: " + str(total)
+		
+	return output
