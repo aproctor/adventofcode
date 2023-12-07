e637a97d43320d4b8b877839427903622cce0564
Adam Proctor <aproctor@gmail.com>
2023-12-07 09:12:49 -0500

    day 4 p1

--- 2023/Godot/Solutions/Day4/day4.gd
+++ 2023/Godot/Solutions/Day4/day4.gd
@@ -13,7 +13,7 @@ func part_1(input) -> String:
 	};
 		
 	var cardRegex = RegEx.new();
-	cardRegex.compile("Card (\\d+): ([0-9 ]+) \\| ([0-9 ]+)")
+	cardRegex.compile("Card +(\\d+): ([0-9 ]+) \\| ([0-9 ]+)")
 	
 	var games = {};
 	
@@ -22,16 +22,18 @@ func part_1(input) -> String:
 		if(result):
 			#output += "%s: [%s][%s]\n" % [result.get_string(1), result.get_string(2), result.get_string(3)]
 			var card_id = result.get_string(1)
-			var winning_numbers = result.get_string(2).strip_edges().split(" ")
-			var card_numbers = result.get_string(3).strip_edges().split(" ")
+			var winning_numbers = result.get_string(2).strip_edges().split_floats(" ")
+			var card_numbers = result.get_string(3).strip_edges().split_floats(" ")
 			var card_score = 0
-			var matches = []
+			var matches = {}
+			
+			#output += "%s vs %s\n" % [winning_numbers, card_numbers]
 			
 			for winner in winning_numbers:
 				for num in card_numbers:
-					if(num == winner && int(num) != 0):
+					if(num == winner && int(num) != 0 && !matches.has(num)):
 						#output += "\tWINNER! [%s][%s]\n" % [num, winner]
-						matches.append(num)
+						matches[num] = true
 						if(card_score == 0):
 							card_score = 1
 						else:
@@ -39,7 +41,7 @@ func part_1(input) -> String:
 							
 			total += card_score
 			
-			output += "Card %s scores %d.  Matching %s\n" % [card_id, card_score, ",".join(matches)]
+			output += "Card %s scores %d.  Matching %s\n" % [card_id, card_score, ",".join(matches.keys())]
 	
 	output += "Total = " + str(total)
 	
