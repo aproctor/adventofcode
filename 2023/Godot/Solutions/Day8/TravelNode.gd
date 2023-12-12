631bcaee3394dd0b40a8cd96e03a36ab15f27419
Adam Proctor <aproctor@gmail.com>
2023-12-12 14:06:12 -0500

    day 8 part 2

--- 2023/Godot/Solutions/Day8/TravelNode.gd
+++ 2023/Godot/Solutions/Day8/TravelNode.gd
@@ -5,9 +5,13 @@ class_name TravelNode
 @export var location = ""
 var left_node : TravelNode = null
 var right_node : TravelNode = null
+@export var start_node : bool = false
+@export var end_node : bool = false
 
 func _init(loc):
 	location = loc
+	start_node = (loc[-1] == "A")
+	end_node = (loc[-1] == "Z")
 
 func connect_to(left, right):
 	left_node = left
--- 2023/Godot/Solutions/Day8/day8.gd
+++ 2023/Godot/Solutions/Day8/day8.gd
@@ -66,4 +66,88 @@ func part_1(input):
 	
 	output += "Steps taken = " + str(total)	
 	finish(output)
+	
+func part_2(input):
+	var total = 0;
+	var output = ""
+	var directions = null
+	
+	travel_map = {}
+		
+	#RLLLLR
+	var dirRegex = RegEx.new();
+	dirRegex.compile("^([RL]+)$")
+	
+	#AAA = (BBB, CCC)
+	var mapRegex = RegEx.new();
+	mapRegex.compile("([0-9A-Z]+) = \\(([0-9A-Z]+), ([0-9A-Z]+)\\)")
+	
+	var start_nodes = []
+	
+	for input_line in input.split("\n"):		
+		var mapResult = mapRegex.search(input_line)
+		if(mapResult):
+			#output += "Node map <%s> <%s> <%s>\n" % [mapResult.get_string(1),mapResult.get_string(2),mapResult.get_string(3)]
+			var parent = _find_or_create_node(mapResult.get_string(1))
+			var left = _find_or_create_node(mapResult.get_string(2))
+			var right = _find_or_create_node(mapResult.get_string(3))
+			parent.connect_to(left, right)
+			
+			if(parent.start_node):
+				start_nodes.append(parent)
+		
+		var dirResult = dirRegex.search(input_line)
+		if(dirResult):
+			output += "directions: [%s]\n" % [dirResult.get_string(1)]
+			directions = dirResult.get_string(1)
+			continue
+	
+	
+	var path_lengths = []
+	for n in start_nodes:
+		output += "Evaluate path for %s\n" % [n.location]
+		var path_length = 0
+		var cur_node = n
+		
+		for i in 1000000: #depth limit on endless navigation		
+			var direction = directions[i % directions.length()]
+			cur_node = cur_node.node_for_direction(direction)
+			#output += "%d: Going %s to %s\n" % [i, direction, cur_node.location]
+			print("%d: Going %s to %s" % [i, direction, cur_node.location])
+		
+			if(i % 100 == 0):
+				progress_updated.emit(float(i%directions.length())/directions.length())
+				output_updated.emit(output)
+				await get_tree().process_frame
+		
+			if(cur_node.end_node):  #probably start node
+				path_length = i + 1
+				break
+		
+		path_lengths.append(path_length)
+	
+	output += "Paths = %s\n" % [str(path_lengths)]
+	
+	var lcm = find_lcm_of_array(path_lengths)
+	output += "Cycle length = %d" % [lcm]
+	
+	#output += "Steps taken = " + str(total)	
+	finish(output)
 
+func find_gcd(x, y) -> int:
+	while y != 0:
+		var temp = y
+		y = x % y
+		x = temp
+	return x
+
+func find_lcm(x, y) -> int:
+	return abs(x * y) / find_gcd(x, y)
+
+# assumes array length is at least >= 2
+func find_lcm_of_array(arr) -> int:		
+	var lcm_result = arr[0]
+	for i in range(1, arr.size()):
+		lcm_result = find_lcm(lcm_result, arr[i])
+	
+	return lcm_result
