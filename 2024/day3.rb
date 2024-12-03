fc7068ec5e04afdaeda8478e431c2a242a31022b
Adam Proctor <aproctor@gmail.com>
2024-12-03 14:26:37 -0500

    day3

--- 2024/day3.rb
+++ 2024/day3.rb
@@ -1,18 +1,29 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2024/day/3
 
+def part1_multiply_all_matches(multiples, verbose = false)
+  total = 0
+  buffer = []
+
+  multiples.each do |m|
+    total += m[0].to_i * m[1].to_i
+    buffer << "#{m[0]}*#{m[1]}"
+  end
+
+  puts buffer.join(" + ") + " = #{total}" if verbose
+
+  total
+end
+
+multiples = []
 File.open('day3.data').each do |line|
   next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
+  md = line.scan(/mul\(([0-9]{1,3}),([0-9]{1,3})\)/)
   if(!md.nil?)
-    puts md[1]
+    multiples.concat md
   end
 end
 
-# class Node
-#   attr_accessor :val
-
-#   def initialize(val)
-#     @val = val
-#   end
-# end
+puts "Part 1"
+p1_total = part1_multiply_all_matches(multiples, true)
+puts p1_total
