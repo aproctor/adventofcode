a743ca68aca585066b6e397c504448413db202a2
Adam Proctor <aproctor@gmail.com>
2024-12-07 21:23:17 -0500

    day 5 p1

--- 2024/day5.rb
+++ 2024/day5.rb
@@ -1,18 +1,49 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2024/day/5
 
+instructions = []
+reports = []
+done_instructions = false
+
 File.open('day5.data').each do |line|
   next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
-  if(!md.nil?)
-    puts md[1]
+
+  line.strip!
+
+  if done_instructions == false
+    if line == ""
+      done_instructions = true
+    else
+      instructions << line.split("|").map(&:to_i)
+    end
+  else
+    reports << line.split(",").map(&:to_i)
   end
 end
 
-# class Node
-#   attr_accessor :val
+#puts "instructions:\n#{instructions.inspect}"
+#puts "reports:\n#{reports.inspect}"
 
-#   def initialize(val)
-#     @val = val
-#   end
-# end
+def report_valid?(report, instructions)
+  instructions.each do |instruction|
+    v1index = report.index(instruction[0])
+    v2index = report.index(instruction[1])
+    next if(v1index.nil? || v2index.nil?)
+
+    return false if v1index > v2index
+  end
+
+  return true
+end
+
+puts "Part 1"
+total = 0
+reports.each do |report|
+  valid = report_valid?(report, instructions)
+  if valid
+    puts report.join(',')
+    #increase total by mid value of report
+    total += report[report.length / 2] if valid
+  end
+end
+puts "Total: #{total}"
