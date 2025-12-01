260bf8e517db8a4666cb0de46d2396af19565283
Adam Proctor <aproctor@gmail.com>
2025-12-01 11:24:19 -0500

    2025

---
+++ 2025/day20.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# See http://adventofcode.com/2025/day/20
+
+File.open('day20.data').each do |line|
+  next if(line.nil?)
+  md = line.match(/day ([0-9]+)/)
+  if(!md.nil?)
+    puts md[1]
+  end
+end
+
+# class Node
+#   attr_accessor :val
+
+#   def initialize(val)
+#     @val = val
+#   end
+# end
