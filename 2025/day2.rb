94901c2565af8395289c96df58efc80a79f78e35
Adam Proctor <aproctor@gmail.com>
2025-12-02 10:04:28 -0500

    day 2 p1

--- 2025/day2.rb
+++ 2025/day2.rb
@@ -1,18 +1,33 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2025/day/2
 
+def valid_id(number)
+  chars = number.to_s.chars
+  if chars.length % 2 != 0
+    return true
+  end
+
+  # split string into two substrings
+  mid = chars.length / 2
+  first_half = chars[0...mid]
+  second_half = chars[mid..-1]
+
+  return first_half != second_half
+end
+
+
+invalid_ids = []
 File.open('day2.data').each do |line|
   next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
-  if(!md.nil?)
-    puts md[1]
+  md = line.strip.split(',').each do |range|
+    vals = range.split('-').map(&:to_i)
+    (vals[0]..vals[1]).each do |i|
+      if !valid_id(i)
+        invalid_ids << i
+        puts "#{i} is an invalid id"
+      end
+    end
   end
 end
 
-# class Node
-#   attr_accessor :val
-
-#   def initialize(val)
-#     @val = val
-#   end
-# end
+puts "Part 1: The sum of all invalid ids is #{invalid_ids.sum}"
