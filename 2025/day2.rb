d10ec0163a237777c1eb2b55ab82b7c3c6e467c9
Adam Proctor <aproctor@gmail.com>
2025-12-02 13:14:26 -0500

    day 2 part 2

--- 2025/day2.rb
+++ 2025/day2.rb
@@ -1,7 +1,23 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2025/day/2
 
-def valid_id(number)
+def get_factors(number, known_factors)
+  return known_factors[number] if known_factors.key?(number)
+  factors = []
+  (1..Math.sqrt(number)).each do |i|
+    if number % i == 0
+      factors << i
+      if i != number / i
+        factors << number / i
+      end
+    end
+  end
+  known_factors[number] = factors.sort
+
+  return factors.sort
+end
+
+def p1_valid_id(number)
   chars = number.to_s.chars
   if chars.length % 2 != 0
     return true
@@ -15,19 +31,51 @@ def valid_id(number)
   return first_half != second_half
 end
 
+def p2_valid_id(number, known_factors)
+  chars = number.to_s.chars
+  len = chars.length
+  factors = get_factors(len, known_factors)
+  if factors.length < 2
+    return true
+  end
 
-invalid_ids = []
+  factors.each do |f|
+    next if f == len
+
+    segment = chars[0...f].join('')
+    target = ""
+    (len / f).times do |i|
+      target += segment
+    end
+
+    if target.to_i == number
+      return false
+    end
+  end
+
+  return true
+end
+
+factors_cache = {}
+p1_invalid_ids = []
+p2_invalid_ids = []
 File.open('day2.data').each do |line|
   next if(line.nil?)
   md = line.strip.split(',').each do |range|
     vals = range.split('-').map(&:to_i)
     (vals[0]..vals[1]).each do |i|
-      if !valid_id(i)
-        invalid_ids << i
-        puts "#{i} is an invalid id"
+      if !p1_valid_id(i)
+        p1_invalid_ids << i
+        #puts "#{i} is an invalid id for part 1"
+      end
+
+      if !p2_valid_id(i, factors_cache)
+        #puts "#{i} is an invalid id for part 2"
+        p2_invalid_ids << i
       end
     end
   end
 end
 
-puts "Part 1: The sum of all invalid ids is #{invalid_ids.sum}"
+puts "Part 1: The sum of all invalid ids is #{p1_invalid_ids.sum}"
+puts "Part 2: The sum of all invalid ids is #{p2_invalid_ids.sum}"
