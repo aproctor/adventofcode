7f28996ec7b37b1994cbcf15c7b85378a5cb116d
Adam Proctor <aproctor@gmail.com>
2025-12-04 01:49:32 -0500

    day 4 p1

--- 2025/day4.rb
+++ 2025/day4.rb
@@ -1,18 +1,68 @@
 #!/usr/bin/env ruby
 # See http://adventofcode.com/2025/day/4
 
-File.open('day4.data').each do |line|
-  next if(line.nil?)
-  md = line.match(/day ([0-9]+)/)
-  if(!md.nil?)
-    puts md[1]
-  end
+def coord(x, y)
+  "#{x},#{y}"
 end
 
-# class Node
-#   attr_accessor :val
+def count_neighbors(x1, y1, rolls_map)
+  count = 0
+  (-1..1).each do |dy|
+    (-1..1).each do |dx|
+      next if(dx == 0 && dy == 0)
 
-#   def initialize(val)
-#     @val = val
-#   end
-# end
+      pos = coord(x1 + dx, y1 + dy)
+      if rolls_map.key?(pos)
+        count += 1
+      end
+    end
+  end
+
+  count
+end
+
+rolls_map = {}
+y = 0
+x = 0
+max_x = 0
+File.open('day4.data').each do |line|
+  next if(line.nil?)
+
+  x = 0
+  line.strip!.each_char.with_index do |char, index|
+    if char == '@'
+      rolls_map[coord(x, y)] = char
+    end
+
+    x += 1
+  end
+  max_x = [max_x, x].max
+
+  #puts "#{y}: "  + line
+
+  y += 1
+end
+
+max_y = y
+
+p1_total = 0
+max_y.times do |cy|
+  #print "#{cy}: "
+  max_x.times do |cx|
+    pos = coord(cx, cy)
+    if rolls_map.key?(pos)
+      count = count_neighbors(cx, cy, rolls_map)
+      if count < 4
+        p1_total += 1
+        print "x"
+      else
+        print rolls_map[pos]
+      end
+    else
+      print "."
+    end
+  end
+  puts ""
+end
+
+puts "Part 1: #{p1_total}"
